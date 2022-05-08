c3df4634776a0ee35112aaf909e500306ab89022
tyrese951@hotmail.fr <tyrese951@hotmail.fr>
2022-05-08 23:02:58 +0200

    Create image.php
    création de la bbd sur mysqladmin

---
+++ image.php
@@ -0,0 +1,24 @@
+<?php
+    if 
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- test pour les images vers la bdd avec l'utilisation de LONGBLOB -->
+    <form name="fo" method="post" action="" enctype="multipart/form-data" >
+     <input type="file"name="image"><br>
+     <input type="submit" name="valider" value="Télecharger" />
+     
+
+</body>
+</html>
